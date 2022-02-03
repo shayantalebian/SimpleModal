ede2b070892c17ea951813ef5e97b3d787c0d8f1
Shayan Talebian <shayantalebianwork@gmail.com>
2022-02-03 18:00:25 +0330

    Create click actino

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const closeModalButton = document.querySelector(".close-modal");
 const openModalButton = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModel = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < openModalButton.length; i++) {
+  openModalButton[i].addEventListener("click", openModel);
+}
+
+closeModalButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
